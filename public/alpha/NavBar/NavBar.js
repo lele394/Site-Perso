eb8b0e9cee69362f9a4003c9bcbcacb2acf3a445
lele394 <lele394prod@gmail.com>
2023-01-08 01:17:23 +0100

    menu is functional now

--- public/alpha/NavBar/NavBar.js
+++
@@ -1,9 +0,0 @@
-const shuttle = document.querySelector('#NBshuttle')
-
-
-
-window.addEventListener('scroll', function(e){
-
-  let pos = (Math.round(document.documentElement.scrollTop / window.innerHeight - 0.2)*2+1) - 2 ; //-2 skip la premiere section et -0.2 offset le switch de catégorie
-    shuttle.style.top = "calc(100vh/8 * "+ pos+" - 3mm)";
-})
